9be04342cae7de0b6d053573d3d9b3de59a441e2
YesCoach <sbsii1020@gmail.com>
2023-08-11 16:17:22 +0900

    Initial Commit

---
+++ SeSAC_TMDBProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_TMDBProject
+//
+//  Created by 박태현 on 2023/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
